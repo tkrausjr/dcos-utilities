0accba81e4ce1ab0f15d60536565bd13d2678a7a
tkrausjr <tkrausjr@gmail.com>
2016-02-08 11:19:02 -0500

    Fixed DF Check

--- dev/dcos_df_check.sh
+++ dev/dcos_df_check.sh
@@ -3,8 +3,7 @@
 
 # Check to see that the /var partition has enough space
 varFree=$(df -g /var | tail -1 | awk '{print $4}')
-if [ $varFree -ge  5]
-then
+if [ $varFree -ge 5 ]; then
   varFreeStatus="PASS"
 else
   varFreeStatus="FAIL"
